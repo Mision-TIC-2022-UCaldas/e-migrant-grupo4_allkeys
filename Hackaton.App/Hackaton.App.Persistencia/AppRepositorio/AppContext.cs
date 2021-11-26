7517d894d2ab7a58cafbdc1069d7554a62071102
JulianMontu <julamontufar@misena.edu.co>
2021-11-26 09:12:06 -0500

    Reto 0 Hackaton

--- Hackaton.App/Hackaton.App.Persistencia/AppRepositorio/AppContext.cs
+++ Hackaton.App/Hackaton.App.Persistencia/AppRepositorio/AppContext.cs
@@ -7,7 +7,7 @@ namespace ProyectoCiclo3.App.Persistencia
         public DbSet<IMC> IMC { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
             if(!optionsBuilder.IsConfigured){
-                optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = ProyectoCiclo3");
+                optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = RetoInicialHackaton");
             }
         }
     }
